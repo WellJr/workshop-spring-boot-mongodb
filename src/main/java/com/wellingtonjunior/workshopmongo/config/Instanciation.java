738ceff481f46f5b8d290c2ec0cc15e520798913
Wellington Junior <wtc-jr@outlook.com>
2023-01-05 07:22:23 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/wellingtonjunior/workshopmongo/config/Instanciation.java
@@ -0,0 +1,31 @@
+package com.wellingtonjunior.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.wellingtonjunior.workshopmongo.domain.User;
+import com.wellingtonjunior.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instanciation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
