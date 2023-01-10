e765f86d4bbc851f5e3aaf4405618754e266e658
Wellington Junior <wtc-jr@outlook.com>
2023-01-10 07:30:29 -0300

    Obtendo post por id

---
+++ src/main/java/com/wellingtonjunior/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.wellingtonjunior.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wellingtonjunior.workshopmongo.domain.Post;
+import com.wellingtonjunior.workshopmongo.repository.PostRepository;
+import com.wellingtonjunior.workshopmongo.services.exepetions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não econtrado"));
+	}
+}
